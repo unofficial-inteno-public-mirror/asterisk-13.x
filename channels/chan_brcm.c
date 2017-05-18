unofficial-inteno-public-mirror/asterisk-1.8.x

Avoid DTMF emulation in the core

channels/chan_brcm.c
@@ -51,6 +51,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision: 284597 $")
 
 #include "asterisk/lock.h"
 #include "asterisk/channel.h"
+#include "asterisk/options.h"
 #include "asterisk/cli.h"
 #include "asterisk/config.h"
 #include "asterisk/module.h"
@@ -1881,6 +1882,10 @@ static void *brcm_monitor_packets(void *data)
 						fr.samples = duration;
 						/* Assuming 8000 samples/second - narrowband alaw or ulaw */
 						fr.len = ast_tvdiff_ms(ast_samp2tv(duration, 8000), ast_tv(0, 0));
+					}
+					if (fr.frametype == AST_FRAME_DTMF_END && fr.len < options_dtmfminduration) {
+						/* If the DTMF is too short, expand it to avoid DTMF emulation in the core */
+						fr.len = options_dtmfminduration;
 					}
 					ast_debug(2, "Sending DTMF [%c, Len %d] (%s)\n", fr.subclass.integer, fr.len, (fr.frametype==AST_FRAME_DTMF_END) ? "AST_FRAME_DTMF_END" : (fr.frametype == AST_FRAME_DTMF_BEGIN) ? "AST_FRAME_DTMF_BEGIN" : "AST_FRAME_DTMF_CONTINUE");
 				}

unofficial-inteno-public-mirror/asterisk-13.x

channels/chan_brcm.c
@@ -51,6 +51,7 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision: 284597 $")
 
 #include "asterisk/lock.h"
 #include "asterisk/channel.h"
+#include "asterisk/options.h"
 #include "asterisk/cli.h"
 #include "asterisk/config.h"
 #include "asterisk/module.h"
@@ -1881,6 +1882,10 @@ static void *brcm_monitor_packets(void *data)
 						fr.samples = duration;
 						/* Assuming 8000 samples/second - narrowband alaw or ulaw */
 						fr.len = ast_tvdiff_ms(ast_samp2tv(duration, 8000), ast_tv(0, 0));
+					}
+					if (fr.frametype == AST_FRAME_DTMF_END && fr.len < option_dtmfminduration) {
+						/* If the DTMF is too short, expand it to avoid DTMF emulation in the core */
+						fr.len = option_dtmfminduration;
 					}
 					ast_debug(2, "Sending DTMF [%c, Len %d] (%s)\n", fr.subclass.integer, fr.len, (fr.frametype==AST_FRAME_DTMF_END) ? "AST_FRAME_DTMF_END" : (fr.frametype == AST_FRAME_DTMF_BEGIN) ? "AST_FRAME_DTMF_BEGIN" : "AST_FRAME_DTMF_CONTINUE");
 				}